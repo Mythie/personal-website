873f8e821b0e8c815e0ff2de5fb91a46468f0ffd
Mythie <me@lucasjamessmith.me>
2020-01-17 13:41:03 +1100

    Remove testing posts, add correct post and update icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,10 +27,10 @@ module.exports = {
         name: 'Lucas Smith - Personal Website',
         short_name: 'Lucas Smith',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#FFFFFF',
+        theme_color: '#FFFFFF',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
     {
